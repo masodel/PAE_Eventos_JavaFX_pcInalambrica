ea0d8eae5fd208b474f6158292f84a7ceb40725e
Mauro Delgado <maurods@uamv.edu.ni>
2026-09-02 13:20:31 -0600

    Agrega eventos y menú contextual a lotes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,11 @@ module ni.edu.uam.pae_eventos_javafx_pcinalambrica {
 
     exports ni.edu.uam.pae_eventos_javafx_pcinalambrica;
     exports ni.edu.uam.pae_eventos_javafx_pcinalambrica.controllers;
+    exports ni.edu.uam.pae_eventos_javafx_pcinalambrica.modelos;
 
     opens ni.edu.uam.pae_eventos_javafx_pcinalambrica.controllers
             to javafx.fxml;
+
+    opens ni.edu.uam.pae_eventos_javafx_pcinalambrica.modelos
+            to javafx.base;
 }
--- src/main/java/ni/edu/uam/pae_eventos_javafx_pcinalambrica/Launcher.java
+++ src/main/java/ni/edu/uam/pae_eventos_javafx_pcinalambrica/Launcher.java
@@ -4,6 +4,6 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(PulperiaApplication.class, args);
+        Application.launch(CafeApplication.class, args);
     }
 }
